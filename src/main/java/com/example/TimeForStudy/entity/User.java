e7945fa0c738047b6bc5ad1226d8d58f9dd5f49d
Артём <velikanovart17@gmail.com>
2020-03-15 18:15:03 +0300

    0

--- src/main/java/com/example/TimeForStudy/entity/User.java
+++ src/main/java/com/example/TimeForStudy/entity/User.java
@@ -18,9 +18,6 @@ public abstract class User {
         this.phone = phone;
     }
 
-    public User(String phone) {
-        this.phone = phone;
-    }
 
     public String getPhone() {
         return phone;
