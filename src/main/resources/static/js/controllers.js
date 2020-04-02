a9127ca01e5154ac0ee7577a2cd86862560236dd
narizhypavel145 <narizhy@mail.ru>
2020-04-02 03:39:04 +0300

    local backup

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -70,26 +70,3 @@ app.controller('loginController', function ($scope, $http, $location) {
     }
 });
 
-app.controller('getcontroller', function($scope, $http, $location) {
-    $scope.getfunction = function(){
-        var url = $location.absUrl() + "getallcustomer";
-
-        $http.get(url).then(function (response) {
-            $scope.response = response.data
-        }, function error(response) {
-            $scope.postResultMessage = "Error with status: " +  response.statusText;
-        });
-    }
-});
-
-app.controller('registerController', function($scope, $http, $location) {
-    $scope.getfunction = function(){
-        var url = $location.absUrl() + "getallcustomer";
-
-        $http.get(url).then(function (response) {
-            $scope.response = response.data
-        }, function error(response) {
-            $scope.postResultMessage = "Error with status: " +  response.statusText;
-        });
-    }
-});
