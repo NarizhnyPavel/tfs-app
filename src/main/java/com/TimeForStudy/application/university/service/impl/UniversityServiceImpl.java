41fcb3beeb849e33cbe2fa949992e06dfd13bf50
Artyom <velikanovart17@gmail.com>
2020-04-29 15:55:25 +0300

    TFS - 113 Fix Save University

--- src/main/java/com/TimeForStudy/application/university/service/impl/UniversityServiceImpl.java
+++ src/main/java/com/TimeForStudy/application/university/service/impl/UniversityServiceImpl.java
@@ -57,55 +57,62 @@ public class UniversityServiceImpl implements UniversityService {
         AddUniversityDto addUniversityDto = new AddUniversityDto(addUniversityAndLessonGridDto);
         UniversityEntity universityEntity = new UniversityEntity(addUniversityDto);
         universityRepository.save(universityEntity);
-        LessonGridEntity lessonGridEntity1 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition1(), 1, universityEntity);
-        LessonGridEntity lessonGridEntity2 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition2(), 2, universityEntity);
-        LessonGridEntity lessonGridEntity3 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition3(), 3, universityEntity);
-        LessonGridEntity lessonGridEntity4 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition4(), 4, universityEntity);
-        LessonGridEntity lessonGridEntity5 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition5(), 5, universityEntity);
-        LessonGridEntity lessonGridEntity6 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition6(), 6, universityEntity);
-        LessonGridEntity lessonGridEntity7 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition7(), 7, universityEntity);
-        if (lessonGridEntity1!=null)
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition1() != null) {
+            LessonGridEntity lessonGridEntity1 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition1(), 1, universityEntity);
             lessonGridRepository.save(lessonGridEntity1);
-        if (lessonGridEntity2!=null)
+        }
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition2() != null) {
+            LessonGridEntity lessonGridEntity2 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition2(), 2, universityEntity);
             lessonGridRepository.save(lessonGridEntity2);
-        if (lessonGridEntity3!=null)
+        }
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition3() != null) {
+            LessonGridEntity lessonGridEntity3 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition3(), 3, universityEntity);
             lessonGridRepository.save(lessonGridEntity3);
-        if (lessonGridEntity4!=null)
+        }
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition4() != null) {
+            LessonGridEntity lessonGridEntity4 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition4(), 4, universityEntity);
             lessonGridRepository.save(lessonGridEntity4);
-        if (lessonGridEntity5!=null)
+        }
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition5() != null) {
+            LessonGridEntity lessonGridEntity5 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition5(), 5, universityEntity);
             lessonGridRepository.save(lessonGridEntity5);
-        if (lessonGridEntity6!=null)
+        }
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition6() != null) {
+            LessonGridEntity lessonGridEntity6 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition6(), 6, universityEntity);
             lessonGridRepository.save(lessonGridEntity6);
-        if (lessonGridEntity7!=null)
+        }
+        if (addUniversityAndLessonGridDto.getLessonGridPosition().getPosition7() != null) {
+            LessonGridEntity lessonGridEntity7 = new LessonGridEntity(addUniversityAndLessonGridDto.getLessonGridPosition().getPosition7(), 7, universityEntity);
             lessonGridRepository.save(lessonGridEntity7);
+        }
     }
 
     /**
      * Изменение значений учебного заведения.
      *
-     * @param id идентификатор.
+     * @param id               идентификатор.
      * @param addUniversityDto учебное заведение.
      */
     @Override
     public void updateUniversity(long id, AddUniversityDto addUniversityDto) {
         UniversityEntity updated = universityRepository.findById(id)
                 .orElseThrow(ErrorDescription.UNIVERSITY_NOT_FOUNT::exception);
-        if (addUniversityDto.getName()!=null) {
+        if (addUniversityDto.getName() != null) {
             updated.setName(addUniversityDto.getName());
         }
-        if (addUniversityDto.getLessonDuration()!=0) {
+        if (addUniversityDto.getLessonDuration() != 0) {
             updated.setLessonDuration(addUniversityDto.getLessonDuration());
         }
-        if (addUniversityDto.getWeeks()!=0) {
+        if (addUniversityDto.getWeeks() != 0) {
             updated.setWeeks(addUniversityDto.getWeeks());
         }
-        if (addUniversityDto.getWorkDays()!=null) {
+        if (addUniversityDto.getWorkDays() != null) {
             updated.setWorkDays(addUniversityDto.getWorkDays());
         }
-        if (addUniversityDto.getColor()!=null) {
+        if (addUniversityDto.getColor() != null) {
             updated.setColor(addUniversityDto.getColor());
         }
-        if (addUniversityDto.getLogo()!=null) {
+        if (addUniversityDto.getLogo() != null) {
             updated.setLogotype(addUniversityDto.getLogo());
         }
         //TODO доделать с lessonGrid
