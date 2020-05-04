3c5cc6f83358bbdee5f04fe2c81d805c7966f2e7
Pavel <narizhy@mail.ru>
2020-05-04 22:18:39 +0300

    TFS-215 stable universitySettingsBlock.html

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -7,11 +7,12 @@ function showHide(element_id) {
     settings.style.display = "none";
     var obj = document.getElementById(element_id);
     if (obj.style.display != "block") {
-        obj.style.display = "block"; //Показываем элемент
+        obj.style.display = "block"; 
     }
 }
 
 let university;
+let user;
 app.controller('control', function ($scope, $http) {
     $scope.logoShow = true;
     var config = {
@@ -19,17 +20,15 @@ app.controller('control', function ($scope, $http) {
             'Content-Type': 'application/json'
         }
     };
+    $http.get('/user/1', config).then(function (response) {
+        user = response.data;
+    })
     $http.get('/university/1', config).then(function (response) {
         university = response.data;
-        var color1 = "#" + university.color;
-        var color2 = "" + ( Number(parseInt(university.color, 16) - 35).toString(16) );
-        let regexp = /[a-f0-9]{6}/gi;
-        while (color2.match(regexp) === null) {
-            color2 = "0" + color2;
-        }
-        color2 = "#" + color2;
-        document.getElementById('mainBlockId').style.backgroundColor = color1;
-        document.getElementById('menu').style.backgroundColor = color2;
+        document.getElementById('mainBlockId').style.backgroundColor = '#' + university.color1;
+        document.getElementById('menu').style.backgroundColor = '#' + university.color2;
+        // document.getElementById('timetable').style.backgroundColor = '#' + university.color2;
+        document.querySelector('.page').style.backgroundColor = '#' + university.color2;
         // document.getElementById('menu').style.borderTop = '18px solid ' + color2;
         // alert('18px solid ' + color2);
         // document.styleSheets[0].addRule('.menu-main:after', 'border-top: 18px solid ' + color2, 0);
